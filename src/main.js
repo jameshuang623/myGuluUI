1375ab6ed556c2f611063276580426b9745811cf
jameshuang623 <363753466@qq.com>
2021-08-01 14:03:40 +0800

    创建 button group

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import router from './router'
 import store from './store'
 import Button from "./components/Button";
 import Icon from "./views/Icon";
+import ButtonGroup from "./components/ButtonGroup";
 
 Vue.config.productionTip = false
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
+Vue.component('g-button-group', ButtonGroup)
 
 
 
